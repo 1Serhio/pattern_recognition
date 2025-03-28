6b37c7e684ab12912dc61ea30557004438fbbbd6
Sergey Fokanov <125132889+1Serhio@users.noreply.github.com>
2025-03-28 15:11:09 +0300

    Update 4_mask.cpp

--- 4_mask/4_mask.cpp
+++ 4_mask/4_mask.cpp
@@ -1,9 +1,11 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 
+using namespace cv;  // Теперь можно не писать cv:: перед функциями
+
 int main() {
     // Загружаем изображение
-    cv::Mat image = cv::imread(".../lena.png");
+    Mat image = imread(".../lena.png");
 
     // Проверяем, успешно ли загружено изображение
     if (image.empty()) {
@@ -18,54 +20,54 @@ int main() {
     int halfWidth = width / 2;
 
     // Разделяем изображение на 4 части
-    cv::Mat topLeft = image(cv::Rect(0, 0, halfWidth, halfHeight));
-    cv::Mat topRight = image(cv::Rect(halfWidth, 0, halfWidth, halfHeight));
-    cv::Mat bottomLeft = image(cv::Rect(0, halfHeight, halfWidth, halfHeight));
-    cv::Mat bottomRight = image(cv::Rect(halfWidth, halfHeight, halfWidth, halfHeight));
+    Mat topLeft = image(Rect(0, 0, halfWidth, halfHeight));
+    Mat topRight = image(Rect(halfWidth, 0, halfWidth, halfHeight));
+    Mat bottomLeft = image(Rect(0, halfHeight, halfWidth, halfHeight));
+    Mat bottomRight = image(Rect(halfWidth, halfHeight, halfWidth, halfHeight));
 
     // Применяем разные обработки к каждой части
-    cv::Mat processedTopLeft, processedTopRight, processedBottomLeft, processedBottomRight;
+    Mat processedTopLeft, processedTopRight, processedBottomLeft, processedBottomRight;
 
     // 1. Инвертируем цвета в верхнем левом углу
-    cv::bitwise_not(topLeft, processedTopLeft);
+    bitwise_not(topLeft, processedTopLeft);
 
     // 2. Переводим в оттенки серого в верхнем правом углу
-    cv::cvtColor(topRight, processedTopRight, cv::COLOR_BGR2GRAY);
-    cv::cvtColor(processedTopRight, processedTopRight, cv::COLOR_GRAY2BGR); // Преобразуем обратно в 3 канала
+    cvtColor(topRight, processedTopRight, COLOR_BGR2GRAY);
+    cvtColor(processedTopRight, processedTopRight, COLOR_GRAY2BGR); // Преобразуем обратно в 3 канала
 
     // 3. Увеличиваем контрастность в нижнем левом углу
     processedBottomLeft = bottomLeft * 1.5;
 
     // 4. Применяем размытие в нижнем правом углу
-    cv::GaussianBlur(bottomRight, processedBottomRight, cv::Size(15, 15), 5);
+    GaussianBlur(bottomRight, processedBottomRight, Size(15, 15), 5);
 
     // Создаём пустое изображение для результата
-    cv::Mat resultImage = cv::Mat::zeros(height, width, image.type());
+    Mat resultImage = Mat::zeros(height, width, image.type());
 
     // Вставляем обработанные части в соответствующие области
-    processedTopLeft.copyTo(resultImage(cv::Rect(0, 0, halfWidth, halfHeight)));
-    processedTopRight.copyTo(resultImage(cv::Rect(halfWidth, 0, halfWidth, halfHeight)));
-    processedBottomLeft.copyTo(resultImage(cv::Rect(0, halfHeight, halfWidth, halfHeight)));
-    processedBottomRight.copyTo(resultImage(cv::Rect(halfWidth, halfHeight, halfWidth, halfHeight)));
+    processedTopLeft.copyTo(resultImage(Rect(0, 0, halfWidth, halfHeight)));
+    processedTopRight.copyTo(resultImage(Rect(halfWidth, 0, halfWidth, halfHeight)));
+    processedBottomLeft.copyTo(resultImage(Rect(0, halfHeight, halfWidth, halfHeight)));
+    processedBottomRight.copyTo(resultImage(Rect(halfWidth, halfHeight, halfWidth, halfHeight)));
 
     // Масштабируем изображения для экрана 1920x1080
-    cv::Mat resizedOriginal, resizedProcessed;
-
+    Mat resizedOriginal, resizedProcessed;
+    
     int targetWidth = 900;  // Половина экрана по ширине (~900 пикс.)
     int targetHeight = (image.rows * targetWidth) / image.cols;  // Сохраняем пропорции
 
-    cv::resize(image, resizedOriginal, cv::Size(targetWidth, targetHeight));
-    cv::resize(resultImage, resizedProcessed, cv::Size(targetWidth, targetHeight));
+    resize(image, resizedOriginal, Size(targetWidth, targetHeight));
+    resize(resultImage, resizedProcessed, Size(targetWidth, targetHeight));
 
     // Объединяем два изображения в одно (размещаем их рядом)
-    cv::Mat displayImage;
-    cv::hconcat(resizedOriginal, resizedProcessed, displayImage);
+    Mat displayImage;
+    hconcat(resizedOriginal, resizedProcessed, displayImage);
 
     // Отображаем результат
-    cv::imshow("Оригинал и обработанное изображение", displayImage);
+    imshow("Оригинал и обработанное изображение", displayImage);
 
     // Ожидаем нажатия клавиши перед закрытием окон
-    cv::waitKey(0);
+    waitKey(0);
 
     return 0;
 }
